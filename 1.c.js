20f09b670b231ff60acb823384ef3311820e54f9
PranaliAdsul <pranuadsulst1137@gmail.com>
2023-05-15 02:15:41 +0530

    Solved

--- 1.c.js
+++ 1.c.js
@@ -1,3 +1,8 @@
+// 1.c. Write a function called `sumOfPrimes` that takes a number as input and returns the sum of all prime numbers less than or equal to the input number.
+// ```js
+// console.log(sumOfPrimes(10));
+// 17 // 2 + 3 + 5 + 7 = 17
+// ```
 function sumOfPrimes(n) {
     let sum = 0;
     for (let i = 2; i <= n; i++) {
